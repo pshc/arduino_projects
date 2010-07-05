ff0705683649184336383147e627890f974867d6
Paul Collier <paul@paulcollier.ca>
2010-07-05 00:36:33 -0700

    Try to count incoming data spikes (and fail...)

--- main.c
+++ main.c
@@ -1,14 +1,21 @@
 #include <WProgram.h>
 #include "clockdisplay.h"
 
+static volatile int ctr = 0;
+
+/* TODO: for some reason this gets called continuously except when sending
+         HIGH from the transmitter */
+static void rf_handler(void) {
+	if (++ctr > 9999) ctr = 0;
+}
+
 int main(void) {
 	init();
 	cdisp_setup();
-	unsigned short ctr = 0;
+	attachInterrupt(0, &rf_handler, RISING); /* pin 2 */
 
 	while (1) {
-		cdisp_number(ctr++, 100);
-		if (ctr > 9999) ctr = 0;
+		cdisp_number(ctr, 50);
 	}
 	return 0;
 }
